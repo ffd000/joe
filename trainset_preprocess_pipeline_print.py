cb683f4024dbda575e90ee18911595db0e40eb21
ffd000 <66318502+ffd000@users.noreply.github.com>
2024-04-06 17:31:17 +0300

    Update trainset_preprocess_pipeline_print.py

--- trainset_preprocess_pipeline_print.py
+++ trainset_preprocess_pipeline_print.py
@@ -15,11 +15,6 @@ import librosa, traceback
 from scipy.io import wavfile
 import multiprocessing
 from my_utils import load_audio
-import tqdm
-
-DoFormant = False
-Quefrency = 1.0
-Timbre = 1.0
 
 mutex = multiprocessing.Lock()
 f = open("%s/preprocess.log" % exp_dir, "a+")
@@ -37,19 +32,19 @@ class PreProcess:
     def __init__(self, sr, exp_dir):
         self.slicer = Slicer(
             sr=sr,
-            threshold=-42,
-            min_length=1500,
+            threshold=-40,
+            min_length=800,
             min_interval=400,
             hop_size=15,
-            max_sil_kept=500,
+            max_sil_kept=150,
         )
         self.sr = sr
         self.bh, self.ah = signal.butter(N=5, Wn=48, btype="high", fs=self.sr)
         self.per = 3.0
         self.overlap = 0.3
         self.tail = self.per + self.overlap
-        self.max = 0.9
-        self.alpha = 0.75
+        self.max = 0.95
+        self.alpha = 0.8
         self.exp_dir = exp_dir
         self.gt_wavs_dir = "%s/0_gt_wavs" % exp_dir
         self.wavs16k_dir = "%s/1_16k_wavs" % exp_dir
@@ -58,11 +53,7 @@ class PreProcess:
         os.makedirs(self.wavs16k_dir, exist_ok=True)
 
     def norm_write(self, tmp_audio, idx0, idx1):
-        tmp_max = np.abs(tmp_audio).max()
-        if tmp_max > 2.5:
-            print("%s-%s-%s-filtered" % (idx0, idx1, tmp_max))
-            return
-        tmp_audio = (tmp_audio / tmp_max * (self.max * self.alpha)) + (
+        tmp_audio = (tmp_audio / np.abs(tmp_audio).max() * (self.max * self.alpha)) + (
             1 - self.alpha
         ) * tmp_audio
         wavfile.write(
@@ -70,9 +61,7 @@ class PreProcess:
             self.sr,
             tmp_audio.astype(np.float32),
         )
-        tmp_audio = librosa.resample(
-            tmp_audio, orig_sr=self.sr, target_sr=16000
-        )  # , res_type="soxr_vhq"
+        tmp_audio = librosa.resample(tmp_audio, orig_sr=self.sr, target_sr=16000)#, res_type="soxr_vhq"
         wavfile.write(
             "%s/%s_%s.wav" % (self.wavs16k_dir, idx0, idx1),
             16000,
@@ -81,9 +70,9 @@ class PreProcess:
 
     def pipeline(self, path, idx0):
         try:
-            audio = load_audio(path, self.sr, DoFormant, Quefrency, Timbre)
+            audio = load_audio(path, self.sr)
             # zero phased digital filter cause pre-ringing noise...
-            # audio = signal.filtfilt(self.bh, self.ah, audio)
+            # audio = signal.filtfilt(self.bh, self.ah, audio) 
             audio = signal.lfilter(self.bh, self.ah, audio)
 
             idx1 = 0
@@ -101,14 +90,12 @@ class PreProcess:
                         idx1 += 1
                         break
                 self.norm_write(tmp_audio, idx0, idx1)
-            # println("%s->Suc." % path)
+            println("%s->Suc." % path)
         except:
             println("%s->%s" % (path, traceback.format_exc()))
 
-    def pipeline_mp(self, infos, thread_n):
-        for path, idx0 in tqdm.tqdm(
-            infos, position=thread_n, leave=True, desc="thread:%s" % thread_n
-        ):
+    def pipeline_mp(self, infos):
+        for path, idx0 in infos:
             self.pipeline(path, idx0)
 
     def pipeline_mp_inp_dir(self, inp_root, n_p):
@@ -124,12 +111,12 @@ class PreProcess:
                 ps = []
                 for i in range(n_p):
                     p = multiprocessing.Process(
-                        target=self.pipeline_mp, args=(infos[i::n_p], i)
+                        target=self.pipeline_mp, args=(infos[i::n_p],)
                     )
-                    ps.append(p)
                     p.start()
-                for i in range(n_p):
-                    ps[i].join()
+                    ps.append(p)
+                    for p in ps:
+                        p.join()
         except:
             println("Fail. %s" % traceback.format_exc())
 
